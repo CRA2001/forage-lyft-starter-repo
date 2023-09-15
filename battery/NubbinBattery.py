841f270beb30c523f656f1950f083c3f1e7b5dff
CRA2001 <carlosarro182@gmail.com>
2023-09-15 10:56:37 +0300

    Added classes and refactored code based on class diagram

---
+++ battery/NubbinBattery.py
@@ -0,0 +1,19 @@
+from battery.battery import Battery
+from datetime import datetime,date
+
+class NubbinBattery(Battery):
+    def __init__(self,last_service_date,current_date):
+     super().__init__(last_service_date)
+     self.current_date = current_date
+    
+    def needs_service(self):
+       #date_of_expected_service = add_years_to_date(self.last_service_date,4)
+        try:
+            date_of_expected_service = self.last_service_date.replace(year=self.last_service_Date.year+4)
+        except ValueError:
+            #preserving calendar day (if feb 29th does not exist, set to 28th)
+            date_of_expected_service = self.last_service_date.replace(year=self.last_service_Date.year+4,day=28)
+        return True if date_of_expected_service < self.current_date else False
+
+       
+       
